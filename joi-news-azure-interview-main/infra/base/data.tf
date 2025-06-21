4d4b4cb387640b82547c06a4f88f446260371fdc
mayurkhudegithub <mayurkhudedevops@gmail.com>
2025-06-21 21:15:30 +0530

    update vault value

--- joi-news-azure-interview-main/infra/base/data.tf
+++ joi-news-azure-interview-main/infra/base/data.tf
@@ -1,5 +1,5 @@
 data "vault_generic_secret" "shared_secret" {
-  path = ""https://api.cloud.hashicorp.com/secrets/2023-11-28/organizations/a1df8099-86f5-4508-aa08-cc6be8931a94/projects/2e6fc177-e2b4-4e4b-b84f-bde03945e36c/apps/Azure-Learning/secrets:open""
+  path = "https://api.cloud.hashicorp.com/secrets/2023-11-28/organizations/a1df8099-86f5-4508-aa08-cc6be8931a94/projects/2e6fc177-e2b4-4e4b-b84f-bde03945e36c/apps/Azure-Learning/secrets:open"
 }
 
 data "azurerm_resource_group" "azure-resource" {
