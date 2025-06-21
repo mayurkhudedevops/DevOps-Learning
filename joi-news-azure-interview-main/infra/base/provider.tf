8df35d8bdbf7a23f11f04d668e8ba0ba59ceda29
mayurkhudegithub <mayurkhudedevops@gmail.com>
2025-06-21 21:14:20 +0530

    tf code

--- joi-news-azure-interview-main/infra/base/provider.tf
+++ joi-news-azure-interview-main/infra/base/provider.tf
@@ -1,28 +1,16 @@
-# We strongly recommend using the required_providers block to set the
-# Azure Provider source and version being used
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=2.99"
-    }
-  }
-
-  backend "azurerm" {
-    resource_group_name  = "news4321_rg_joi_interview"
-    storage_account_name = "news4321sajoiinterview"
-    container_name       = "news4321terraformcontainerjoiinterview"
-    key                  = "base/terraform.tfstate"
-  }
-}
-
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
-  features {}
+  tenant_id       = data.vault_generic_secret.shared_secret.data["ARM_TENANT_ID"]
+  subscription_id = data.vault_generic_secret.shared_secret.data["ARM_SUBSCRIPTION_ID"]
+  client_id       = data.vault_generic_secret.shared_secret.data["ARM_CLIENT_ID"]
+  client_secret   = data.vault_generic_secret.shared_secret.data["ARM_CLIENT_SECRET"]
 
+  storage_use_azuread        = true
+  skip_provider_registration = "true"
+  features {
+  }
 }
 
-data "azurerm_resource_group" "azure-resource" {
-  name = "news4321_rg_joi_interview"
+provider "vault" {
+  skip_child_token = true
 }
 
