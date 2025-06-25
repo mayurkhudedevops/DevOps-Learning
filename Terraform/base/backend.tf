0ba5613cb6a1cc084ce2f185cb8b2669aa24ff1f
mayurkhudegithub <mayurkhudedevops@gmail.com>
2025-06-25 10:56:28 +0530

    added backend

---
+++ Terraform/base/backend.tf
@@ -0,0 +1,6 @@
+workspaces {
+  name = "Terraform-Base" # should match with env0 workspace #de-cig-rt-kw-dev
+}
+
+hostname    = "backend.api.env0.com"
+organizaion = "22d2a1ce-1fb7-4127-8c4a-933f2c25d5df"
