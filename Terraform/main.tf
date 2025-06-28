cd634ece252e04322d2391a533f8fd812db0860a
mayurkhudegithub <mayurkhudedevops@gmail.com>
2025-06-28 18:31:55 +0530

    update vnet

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -9,7 +9,7 @@ resource "azurerm_resource_group" "aks_rg" {
 }
 
 # Virtual Network
-resource "azurerm_virtual_network" "aks_vnet" {
+/*resource "azurerm_virtual_network" "aks_vnet" {
   name                = "aks-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.aks_rg.location
@@ -23,6 +23,18 @@ resource "azurerm_virtual_network" "aks_vnet" {
   virtual_network_name = azurerm_virtual_network.aks_vnet.name
   address_prefixes       = ["10.1.1.0/24"]
 
+}*/
+
+resource "azurerm_virtual_network" "aks_vnet" {
+  name                = "myvnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.1.0.0/16"]
+
+  subnet {
+    name           = "aks_subnet"
+    address_prefix = ["10.1.1.0/24"]
+  }
 }
 
 # AKS Cluster
