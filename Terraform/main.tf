398e67d3b283d05720964a28847e86950471dec4
mayurkhudegithub <mayurkhudedevops@gmail.com>
2025-06-28 18:35:20 +0530

    update vnet

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -33,7 +33,6 @@ resource "azurerm_virtual_network" "aks_vnet" {
 
   subnet {
     name           = "aks_subnet"
-    resource_group_name  = azurerm_resource_group.aks_rg.name
     address_prefix = ["10.1.1.0/24"]
   }
 }
