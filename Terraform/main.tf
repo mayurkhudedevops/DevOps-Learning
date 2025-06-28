e5d2fc5a6afc6230a9e8c69ca32681c2e77b9637
mayurkhudegithub <mayurkhudedevops@gmail.com>
2025-06-28 16:53:28 +0530

    update address prefix

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -21,7 +21,7 @@ resource "azurerm_subnet" "aks_subnet" {
   name                 = "aks-subnet"
   resource_group_name  = azurerm_resource_group.aks_rg.name
   virtual_network_name = azurerm_virtual_network.aks_vnet.name
-  address_prefixes     = ["10.0.0.0/28"]
+  address_prefixes     = ["10.240.0.0/28"]
 
   /*delegation {
     name = "aks_delegation"
