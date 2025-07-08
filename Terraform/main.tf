91e903c70b69471406214dfb23c9d37104632416
mayurkhudedevops <mayurkhudedevops@gmail.com>
2025-07-08 13:42:50 +0530

    update tf code

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -17,11 +17,24 @@ resource "azurerm_app_service_plan" "asp" {
   }
 }
 
-resource "azurerm_linux_web_app" "example" {
+resource "azurerm_linux_web_app" "webapp" {
   name                = "test-asp"
   resource_group_name = azurerm_resource_group.rg.name
   location            = "East US"
-  service_plan_id     = azurerm_service_plan.asp.id
+  app_service_plan_id = azurerm_app_service_plan.asp.id
 
-  site_config {}
+  site_config {
+    linux_fx_version = "PYTHON|3.10"
+  }
+
+  app_settings = {
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+    "WEBSITE_RUN_FROM_PACKAGE"            = "1"
+  }
+
+  https_only = true
+}
+
+resource "random_id" "random" {
+  byte_length = 4
 }
