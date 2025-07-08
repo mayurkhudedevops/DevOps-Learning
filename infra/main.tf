733b4c8713e5a353d4fb2128eb4bd66586a95ab4
mayurkhudedevops <mayurkhudedevops@gmail.com>
2025-07-08 13:20:16 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,32 +1,41 @@
-# Configure the AzureRM provider
-# This block tells Terraform that we'll be interacting with Azure.
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0" # Use a compatible version (e.g., "~> 3.0" or "~> 3.90.0")
-    }
-  }
-}
-
-# Provider configuration for Azure
-# This assumes you've already authenticated via Azure CLI (`az login`).
 provider "azurerm" {
-  features {} # Required for current versions of the AzureRM provider
+  features {}
 }
 
-# 1. Create a Resource Group
-# A logical container for your Azure resources.
-resource "azurerm_resource_group" "my_resource_group" {
-  name     = "my-basic-storage-rg"  # Unique name for your resource group
-  location = "East US"              # Choose an Azure region (e.g., "East US", "West Europe", "centralindia")
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-webapp-demo"
+  location = "East US"
 }
 
-# 2. Create an Azure Storage Account
-resource "azurerm_storage_account" "my_storage_account" {
-  name                     = "mybasicstorageacc001" # This name MUST be globally unique (3-24 lowercase letters/numbers)
-  resource_group_name      = azurerm_resource_group.my_resource_group.name
-  location                 = azurerm_resource_group.my_resource_group.location
-  account_tier             = "Standard"             # Performance tier: Standard (general purpose) or Premium
-  account_replication_type = "LRS"                  # Data redundancy: LRS (Locally Redundant Storage - 3 copies in one datacenter)
+resource "azurerm_app_service_plan" "asp" {
+  name                = "appserviceplan-demo"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku {
+    tier = "Basic"
+    size = "B1"
   }
+  os_type = "Linux"
+}
+
+resource "azurerm_app_service" "app" {
+  name                = "webapp-demo-${random_id.random.hex}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.asp.id
+
+  site_config {
+    linux_fx_version = "PYTHON|3.10"
+  }
+
+  app_settings = {
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+    "WEBSITE_RUN_FROM_PACKAGE"            = "1"
+  }
+
+  https_only = true
+}
+
+resource "random_id" "random" {
+  byte_length = 4
+}
